6044a5f1d4c4fbd56d68539859052f4fa7092d26
fisherlyon <fwlyon@calpoly.edu>
2025-01-19 17:38:45 -0800

    janky auxiliary form done

--- tseitin.rkt
+++ tseitin.rkt
@@ -12,7 +12,7 @@
 (struct condF ([l : Formula] [r : Formula]) #:transparent)
 (struct bicondF ([l : Formula] [r : Formula]) #:transparent)
 
-; parses a formula
+; parses a formula (Concrete Syntax -> AST)
 (define (parse [s : Sexp]) : Formula
   (match s
     [(and (not (? invalid-var? var)) (? symbol? var)) (varF var)]
@@ -23,14 +23,48 @@
     [(list '<-> left right) (bicondF (parse left) (parse right))]
     [_ (error 'parse "invalid syntax, given ~e" s)]))
 
-; prepare for tseitin transform
-(define (to-auxiliary [form : Formula] [count : Integer]) : Formula
-  (match count
-    [1 (to-auxiliary
-        (bicondF (auxF (make-var count)) form) (+ 1 count))]
-    [_ form])) ;; TO-DO
+; AST -> Concrete Syntax
+(define (to-sexp [form : Formula]) : Sexp
+  (match form
+    [(varF var) var]
+    [(auxF aux) aux]
+    [(notF sub) (list '! (to-sexp sub))]
+    [(andF forms) (cons '& (map to-sexp forms))]
+    [(orF forms) (cons 'v (map to-sexp forms))]
+    [(condF l r) (list '-> (to-sexp l) (to-sexp r))]
+    [(bicondF l r) (list '<-> (to-sexp l) (to-sexp r))]
+    [_ (error 'to-sexp "invalid AST, given ~e" form)]))
 
-; (to-auxiliary (parse '(-> (v A (! B)) (<-> A (! B)))) 1)
+; converts a formula to complete auxiliary form
+(define (to-auxiliary [form : Formula] [count : Integer]) : Formula
+  (andF (aux-helper form 1)))
+
+; converts formula to incomplete auxiliary form
+(define (aux-helper [form : Formula] [count : Integer]) : (Listof Formula)
+  (match form
+    [(? is-literal?) '()]
+    [(notF sub)
+     (append (list (bicondF (auxF (make-var count)) form))
+             (aux-helper sub (+ 1 count)))]
+    [(or (andF subs) (orF subs))
+     (append (list (bicondF (auxF (make-var count)) form))
+             (list-helper (cast subs (Listof Formula)) (+ 1 count)))]
+    [(or (condF l r) (bicondF l r))
+     (append (list (bicondF (auxF (make-var count)) form))
+             (aux-helper (cast l Formula) (+ 1 count))
+             (aux-helper (cast r Formula) (+ 2 count)))]))
+
+; converts a list of formula to their incomplete auxiliary form
+(define (list-helper [forms : (Listof Formula)] [count : Integer]) : (Listof Formula)
+  (match forms
+    ['() '()]
+    [(cons f r)
+     (if (is-literal? f)
+         (list-helper r (+ count 1))
+         (cons (bicondF (auxF (make-var count)) f) (list-helper r (+ count 1))))]))
+
+; (to-auxiliary (parse '(-> (v A (! B)) (<-> A (! B)))) '() 1 0)
+; (to-sexp (andF (go (parse '(-> (v A (! B)) (-> A (! B)))) 1)))
 
 ; checks if a formula is in CNF
 (define (is-cnf? [form : Formula]) : Boolean
@@ -44,11 +78,10 @@
 (define (make-var [n : Integer]) : Symbol
   (string->symbol (format "x~a" n)))
 
-; checks if a formula is a literal
+; checks if a formula is a literal (assuming auxF not a literal)
 (define (is-literal? [form : Formula]) : Boolean
   (match form
     [(varF _) #t]
-    [(auxF _) #t]
     [(notF (varF _)) #t]
     [_ #f]))
 
@@ -58,6 +91,15 @@
     ['() #t]
     [(cons f r) (if (not (is-literal? f)) #f (literal-list? r))]))
 
+; checks if a formula is made up of just literals
+(define (just-literals? [form : Formula]) : Boolean
+  (match form
+    [(andF forms) (literal-list? forms)]
+    [(orF forms) (literal-list? forms)]
+    [(condF l r) (and (is-literal? l) (is-literal? r))]
+    [(bicondF l r) (and (is-literal? l) (is-literal? r))]
+    [_ #f]))
+
 ; checks if something is an invalid symbol
 (define (invalid-var? [sym : Any]) : Boolean
   (match sym
@@ -65,6 +107,31 @@
     [_ #f]))
 
 ; test cases
+; aux-helper tests
+(check-equal?
+ (to-sexp (to-auxiliary (parse '(-> (v A (! B)) (<-> A (! B)))) 1))
+ '(& (<-> x1 (-> (v A (! B)) (<-> A (! B))))
+     (<-> x2 (v A (! B)))
+     (<-> x3 (<-> A (! B)))))
+
+(check-equal?
+ (to-sexp (to-auxiliary (parse '(-> (-> R P) (-> (! (& Q R)) P))) 1))
+ '(& (<-> x1 (-> (-> R P) (-> (! (& Q R)) P)))
+     (<-> x2 (-> R P))
+     (<-> x3 (-> (! (& Q R)) P))
+     (<-> x4 (! (& Q R)))
+     (<-> x5 (& Q R))))
+
+; just-literals? tests
+(check-equal?
+ (just-literals? (bicondF (auxF 'x1) (condF (auxF 'x2) (auxF 'x3)))) #f)
+(check-equal?
+ (just-literals? (condF (varF 'R) (notF (varF 'P)))) #t)
+(check-equal?
+ (just-literals? (andF (list (varF 'Q) (varF 'R)))) #t)
+(check-equal?
+ (just-literals? (orF (list (varF 'Q) (notF (varF 'R))))) #t)
+
 ; is-cnf? tests
 (check-equal? (is-cnf? (varF 'A)) #t)
 (check-equal? (is-cnf? (orF (list (varF 'A) (varF 'B)))) #t)
